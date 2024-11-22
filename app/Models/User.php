d6412d3eaa0e3ebd6de9a7827ec5dce8f2030f36
RjkzRhen <qwora1@bk.ru>
2024-11-22 19:14:05 +0300

    Добавил ячейки в phone

--- app/Models/User.php
+++ app/Models/User.php
@@ -2,30 +2,25 @@
 
 namespace App\Models;
 
-use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 
 class User extends Authenticatable
 {
-    use HasFactory, Notifiable;
+    use Notifiable;
 
     protected $fillable = [
-        'name',
-        'email',
-        'password',
         'last_name',
         'first_name',
         'middle_name',
+        'age',
+        'username',
+        'password',
     ];
 
     protected $hidden = [
         'password',
         'remember_token',
-    ];
-
-    protected $casts = [
-        'email_verified_at' => 'datetime',
     ];
 
     public function phones()
